93504fbbb88110e1bf756383a29dfaa50ae1d2cb
Surpreet Mundra <surpreet.mundra@gmail.com>
2021-10-28 15:03:02 -0400

    adding alternative solution

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+// solution 1
 let sym = "#"
-for (let number = 0; number <= 7; number++) {
+for (let number = 0; number < 7; number++) {
 	console.log(sym)
     sym = sym + "#"
+}
+
+
+// solution 2 
+for (let sym = "#"; sym.length <= 7; sym += "#") {
+	console.log(sym)
 }
